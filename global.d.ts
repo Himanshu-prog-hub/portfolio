8d623a537a0a54344b16ed041c01a5efe782f611
Himanshu <mishra00.11himanshu@gmail.com>
2025-01-10 15:54:30 +0530

    Completed the dirst page and fiex hydration error bug just before adding my projects

---
+++ global.d.ts
@@ -0,0 +1,9 @@
+import 'three-globe';
+import { Object3DNode } from '@react-three/fiber';
+import ThreeGlobe from 'three-globe';
+
+declare module '@react-three/fiber' {
+  interface ThreeElements {
+    threeGlobe: Object3DNode<ThreeGlobe, typeof ThreeGlobe>;
+  }
+}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true, // Enable or disable strict mode
+  swcMinify: true, // Use SWC for minification
+  compiler: {
+    styledComponents: true, // Example: styled-components support
+  },
+  webpack: (config) => {
+    // Add any custom webpack configurations here
+    return config;
+  },
 };
 
 export default nextConfig;
+
